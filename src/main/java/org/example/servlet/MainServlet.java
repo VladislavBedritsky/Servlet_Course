0acd3dcd6a10b88ad79b6d026cf04057728833af
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-05-28 16:30:09 +0300

    jetty plugin is added

--- src/main/java/org/example/servlet/MainServlet.java
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -12,15 +12,24 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        log("Method init");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
+        resp.getWriter().write("Method service\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.getWriter().write("Method doGet\n");
     }
 
     @Override
     public void destroy() {
         super.destroy();
+        log("Method destroy");
     }
 }
